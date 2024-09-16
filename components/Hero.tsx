13c965be39830ed4cbe42295ec397e0b9d1ec19e
chiefpat450119 <91508703+chiefpat450119@users.noreply.github.com>
2024-09-15 23:14:10 -0700

    hero page updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,18 +9,20 @@ const Hero = () => {
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
-          src="camp.svg"
-          alt="camp"
+          src="tennis_ball.svg"
+          alt="tennis ball"
           width={50}
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Tennis Coaching for Everyone</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          We want to be on each of your journeys seeking the satisfaction of
-          seeing the incorruptible beauty of nature. We provide a solution for
-          you to be able to use our application when climbing, yes offline maps
-          you can use at any time there is no signal at the location.
+          Ever wanted to learn tennis, but found yourself struggling to hit the
+          ball? Have you been frustrated trying to play only to spend most of
+          the time picking up balls? With CanDo Tennis, you can begin with the
+          very basics and develop your game step-by-step, following our
+          specially curated lessons to elevate your play to the next level.
+          Schedule your lessons now to get started!
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
@@ -38,7 +40,7 @@ const Hero = () => {
               ))}
           </div>
           <p className="bold-16 lg:bold-20 text-blue-70">
-            198k
+            10+
             <span className="regular-16 lg:regular-20 ml-1">
               Excellent Reviews
             </span>
@@ -57,7 +59,6 @@ const Hero = () => {
       </div>
       <div className="relative flex flex-1 items-start">
         <div className="relative z-20 flex flex-col gap-8 rounded-3xl bg-green-90 w-[268px] px-7 py-8">
-
           <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
@@ -76,7 +77,6 @@ const Hero = () => {
               <p className="bold-20 text-white">2.040 km</p>
             </div>
           </div>
-
         </div>
       </div>
     </section>
